a39331d7f17b6d463e4c2e1be6ac636a807f9a22
RexOLiPb <52637797+RexOLiPb@users.noreply.github.com>
2023-04-05 05:40:38 +0000

    fix

--- js/index.js
+++ js/index.js
@@ -1,18 +1,32 @@
 const audio = document.getElementById("myAudio");
 const myAudio = new Audio("images/One Last Kiss.mp3");
-const musicPlayer = document.querySelector('#music-player');
+const musicPlayer = document.querySelector("#music-player");
 const icon = document.querySelector("#music");
-let isplay = false
-musicPlayer.addEventListener('click', () => {
-    if(isplay){
-        myAudio.pause();
-        isplay=false
-        icon.className="fa-solid fa-volume-xmark"
+const skipEl = document.querySelector(".skip");
 
-    }else{
-        myAudio.play();
-        isplay=true
-        icon.className="fa-solid fa-volume-high"
-    }
+let isplay = false;
+musicPlayer.addEventListener("click", () => {
+  if (isplay) {
+    myAudio.pause();
+    isplay = false;
+    icon.className = "fa-solid fa-volume-xmark";
+  } else {
+    myAudio.play();
+    isplay = true;
+    icon.className = "fa-solid fa-volume-high";
+  }
+});
 
-})
+window.addEventListener("scroll", function () {
+  if (window.pageYOffset > 50) {
+    skipEl.classList.add("show");
+  } else {
+    skipEl.classList.remove("show");
+  }
+});
+
+document.addEventListener("keydown", function (event) {
+  if (event.keyCode === 9) {
+    skipEl.classList.add("show");
+  }
+});
